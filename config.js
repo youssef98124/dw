13e4206bd25b8b7a4d34b7bceaa5fb7ae8d8dcdf
youssef98124 <38530808+youssef98124@users.noreply.github.com>
2018-04-25 01:18:49 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'NDM4NDYzNzI3NDUzNDcwNzIw.DcE-tw.ye2pk84afgHVPT47Ggsvj1nccKs'; // ya wr3 la tns5
+
+exports.PREFIX = '2';
+
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
